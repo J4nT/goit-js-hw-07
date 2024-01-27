eaeed7457a92551d20d13c4a396a2233b01465e0
J4nT <145610618+J4nT@users.noreply.github.com>
2024-01-27 18:36:27 +0100

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
-import { galleryItems } from './gallery-items.js';
+/* import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+const gallery = document.querySelector(".gallery");
+const galleryItemsNew = galleryItems.join("").map(
+    (galleryItem) => <li class="galleryItem">
+        <a class="gelleryLink" href="${galleryItem.original}">
+            <img class="galleryImage"
+                src="${galleryItem.preview}"
+                alt="${galleryItem.description}"
+                data-source="${galleryItem.original}">
+            </img>
+        </a>
+    </li>
+);
+gallery.insertAdjacentHTML("afterbegin", galleryItemsNew);
 console.log(galleryItems);
+ */
+//Dostęp do dokumentu
+const gallery = document.querySelector(".gallery");
+//Tworzenie tablicy danych i znaczników galerii
+const listGalleryItems = galleryItems.map(
+  (galleryItem) =>
+    `<li class="gallery__item">
+      <a class="gallery__link" href="${galleryItem.original}">
+      <img
+        class="gallery__image"
+        src="${galleryItem.preview}" 
+        alt="${galleryItem.description}"
+        data-source="${galleryItem.original}"
+        >
+        </img>
+        </a>
+     </li>`
+).join('');
+//Otwieranie okna modalnego po kliknięciu
+document.querySelector('.gallery').addEventListener('click', function (event) {
+    if (event.target.tagName === 'IMG') {
+    event.preventDefault();
+    const imageUrl = event.target.dataset.source;
+    openModal(imageUrl);
+  }
+});
+function openModal(imageUrl) {
+  const instance = basicLightbox.create(`<img src="${imageUrl}" alt="Image">`);
+  instance.element().addEventListener('click', function (event) {
+    if (event.target === instance.content) {
+      instance.close();
+    }
+  });
+    window.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+      instance.close();
+    }
+  });
+  
+  instance.show();
+}
